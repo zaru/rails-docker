8e17151c9534de441b31b45f5ca0f45371236d75
zaru@sakuraba <zarutofu@gmail.com>
2017-08-13 00:44:35 +0900

    Added scope

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -4,6 +4,12 @@ resource "google_container_cluster" "primary" {
   initial_node_count = 3
 
   node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
     machine_type = "n1-standard-1"
   }
 }
