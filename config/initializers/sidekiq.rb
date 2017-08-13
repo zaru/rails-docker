86f8f628bdabf45301a249a818d8538627a53d94
zaru@sakuraba <zarutofu@gmail.com>
2017-08-13 23:33:33 +0900

    Fixed redis host

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://redis:6379/1' }
+  config.redis = { url: 'redis://127.0.0.1:6379/1' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://redis:6379/1' }
+  config.redis = { url: 'redis://127.0.0.1:6379/1' }
 end
